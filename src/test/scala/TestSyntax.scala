35da6ca83f98917fd8e80bafe12bf262ae64bf97
sang dang <dnsang1010@gmail.com>
2020-03-03 10:57:50 +0700

    update

--- src/test/scala/TestSyntax.scala
+++ src/test/scala/TestSyntax.scala
@@ -4,17 +4,6 @@ object TestSyntax {
   def main(args: Array[String]): Unit = {
 
 
-    val data = Array(Array(1, 2), Array(3, 4))
-
-    val flatten = data.flatten
-    flatten.foreach(print(_))
-
-
-    val data2 = Array((1, 2), (3, 4))
-
-    val flatten2 = data2.flatMap(f => Array(f._1,f._2))
-    flatten2.foreach(print(_))
-
   }
 
 }
